befe1745e0c5fcc7ee1954e982cc800b620d4a39
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-04-26 14:01:22 -0700

    implementation of a simple shell

--- 8_str_ops0.c
+++ 8_str_ops0.c
@@ -1,12 +1,11 @@
 #include "shell.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * _strcmp - compares two strings
  * @str1: string 1
  * @str2: string 2
- * Return: a negative num if str1 < str2, positive if str1 > str2 or zero if str1 == str2
+ * Return: a negative or a postive number or zero
  */
 int _strcmp(char *str1, char *str2)
 {
@@ -15,13 +14,11 @@ int _strcmp(char *str1, char *str2)
 
 	s1 = str1;
 	s2 = str2;
-
 	if (!str1 && !str2)
 		return (0);
 
 	if (!str1 || !str2)
 		return (-1);
-
 	while (*s1 == *s2)
 	{
 		if (*s1 == '\0')
@@ -31,15 +28,12 @@ int _strcmp(char *str1, char *str2)
 		s1 = s1 + 1;
 		s2 = s2 + 1;
 	}
-
 	if (*s1 < *s2)
 		return (*s1 - *s2);
 	if (*s1 > *s2)
 		return (*s1 - *s2);
-
 	return (0);
 }
-
 /**
  * _strlen - get the length of a string
  * @s: the string
@@ -54,9 +48,8 @@ int _strlen(char *s)
 		len++;
 	return (len);
 }
-
 /**
-  * word_count - counts the number of words in a string seperated by a delim
+  * word_count - counts the number of words seperated by a delim
   * @str: string to count the words
   * @delim: separator between words
   * Return: number of words
@@ -77,10 +70,8 @@ int word_count(char *str, char *delim)
 	}
 	if (str && str[0])
 		counter++;
-
 	return (counter);
 }
-
 /**
  * _strcat - Concats two strings
  * @dest: The destination
@@ -110,7 +101,6 @@ char *_strcat(char *dest, char *src)
 	*wr = '\0';
 	return (dest);
 }
-
 /**
   * _strcpy - copy src into dest
   * @dest: destination for copied string
@@ -125,6 +115,5 @@ char *_strcpy(char *dest, char *src)
 	{
 		dest[x] = src[x];
 	}
-
 	return (dest);
 }
